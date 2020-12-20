0781181844eafe0cd44b3f74f2da47eec2cc81ca
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2020-12-20 18:25:29 +0000

    Delete list view, replace with recycler view

--- app/src/main/java/com/example/notekeeper/NoteListActivity.kt
+++ app/src/main/java/com/example/notekeeper/NoteListActivity.kt
@@ -2,10 +2,8 @@ package com.example.notekeeper
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import kotlinx.android.synthetic.main.content_note_list.*
 
 class NoteListActivity : AppCompatActivity() {
 
@@ -19,25 +17,11 @@ class NoteListActivity : AppCompatActivity() {
             startActivity(activityIntent)
         }
 
-        // Populate the list
-        listNotes.adapter = ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,
-                DataManager.notes)
 
-        // Allow selection of a note to bring up the edit note screen
-        listNotes.setOnItemClickListener { parent, view, position, id ->
-            // Create the Intent
-            val activityIntent = Intent(this, MainActivity::class.java)
-            // Include the position of the selected note
-            activityIntent.putExtra(NOTE_POSITION, position)
-            // start activity
-            startActivity(activityIntent)
-        }
     }
 
     override fun onResume() {
         super.onResume()
-        // Ensure the list is updated when we return to this activity
-        (listNotes.adapter as ArrayAdapter<NoteInfo>).notifyDataSetChanged()
+        
     }
 }
